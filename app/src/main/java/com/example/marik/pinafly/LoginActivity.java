ef0e9391530ce754a375a95b4a4325a64576fe13
Marat <myelemes@gmail.com>
2014-10-14 12:23:34 -0400

    Pinafly App in progress

---
+++ app/src/main/java/com/example/marik/pinafly/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.marik.pinafly;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+    private EditText  username=null;
+    private EditText  password=null;
+    private TextView attempts;
+    private Button login;
+    int counter = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        attempts = (TextView)findViewById(R.id.textView5);
+        attempts.setText(Integer.toString(counter));
+        login = (Button)findViewById(R.id.button1);
+    }
+
+    public void login(View view){
+        if(username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")){
+            Toast.makeText(getApplicationContext(), "Redirecting...",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+            attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if(counter==0){
+                login.setEnabled(false);
+            }
+
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+}
