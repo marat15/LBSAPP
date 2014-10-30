9a1d8982ef599c42f9ded00796d5a779677d7e98
Marat <myelemes@gmail.com>
2014-10-30 19:34:19 -0400

    Got the map activity working

--- app/src/main/java/com/example/marik/pinafly/MapActivity.java
+++ app/src/main/java/com/example/marik/pinafly/MapActivity.java
@@ -3,15 +3,17 @@ package com.example.marik.pinafly;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.model.Marker;
 
 
-public class GoogleMap extends Activity {
+public class MapActivity extends Activity {
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
     }
 
 }
